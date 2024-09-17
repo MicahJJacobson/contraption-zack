5daa91c00e7fc50bf49c036446ac883fbc9d186c
JJ Gillis <jgillis0022@pointloma.edu>
2024-09-17 13:41:23 -0700

    no message

--- Player.java
+++ Player.java
@@ -14,10 +14,6 @@ public class Player extends Draw
    {
    //Creating the ovals with black outline around them
       gc.setFill(Color.RED);
-<<<<<<< HEAD
       gc.fillOval(x,y,50,50);
-=======
-      gc.fillOval(x,y,27,27);
->>>>>>> 1997a02d0ecdb7dc8e11221fac688f50e25949a9
    }
 }
