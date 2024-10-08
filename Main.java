7abbd479ce247cd68b04ed954661424bd1127429
mikelecharri <102259614+mikelecharri@users.noreply.github.com>
2024-10-08 12:50:36 -0700

    Can end game

--- Main.java
+++ Main.java
@@ -109,6 +109,7 @@ public class Main extends Application
    */
    int currentRoom = 0;
    int currentRoomPrev = -1; //for level switching
+   boolean twoStart = false; // checks if two starting positions
    //String direction = "left";
    /*
    Spike newSpike = new Spike(684, 134, true, Color.GREEN);
@@ -125,7 +126,7 @@ public class Main extends Application
 
 
    //intially 1st level
-   String levelFile = "6thLevel.txt";
+   String levelFile = "9thLevel.txt";
 
    public void start(Stage stage)
    {
@@ -148,10 +149,6 @@ public class Main extends Application
             levelSwitches.get(i).add(null);
          }
       }
-
-   String levelFile = "8thLevel.txt";
-
-
    
       initializeArrayLists();
       
@@ -551,8 +548,25 @@ public class Main extends Application
             boundariesR = boundaries.get(currentRoom).get(3);
             if(currentRoom>currentRoomPrev) 
             {
-               player.setX(Position.get(currentRoom).get(0)); //start x
-               player.setY(Position.get(currentRoom).get(1)); //start y
+               
+               if(twoStart) 
+               {
+                 /* if(Position.get(currentRoom-1).get(2) == ) 
+                  { 
+                     player.setX(Position.get(currentRoom).get(4)); //start x
+                     player.setY(Position.get(currentRoom).get(5)); //start y 
+                  }
+                  else
+                  {
+                     player.setX(Position.get(currentRoom).get(0)); //start x
+                     player.setY(Position.get(currentRoom).get(1)); //start y  
+                  }  */
+               }
+               else
+               {
+                  player.setX(Position.get(currentRoom).get(0)); //start x
+                  player.setY(Position.get(currentRoom).get(1)); //start y 
+               }
             }
             else if(currentRoom<currentRoomPrev)
             {
